66e9d25ce737897f11729fe865254342f1df6a07
HanaHww2 <hanahww2@gmail.com>
2025-02-13 17:31:09 +0900

    build: 프로젝트 초기 세팅

---
+++ src/test/java/com/ioteam/order_management_platform/OrderManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioteam.order_management_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
