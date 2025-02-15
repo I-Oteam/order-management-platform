adec043e22f7c54243244eb5d9f3d1891dbbf113
HanaHww2 <hanahww2@gmail.com>
2025-02-15 10:42:47 +0900

    feat: review - 리뷰 삭제하기 테스트 추가#6
    - 테스트 환경 구축(임베디드 h2 디비 사용)
    - 목 데이터 추가를 위한 data.sql 추가 및 설정

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO review
+    (is_public, review_score, created_at, deleted_at, modified_at, created_by, deleted_by, modified_by, review_id, review_image_url, review_content)
+VALUES
+    (true, 5, '2025-02-14 21:04:14.162', NULL, '2025-02-14 21:04:14.162', 'd2ed72d8-090a-4efb-abe4-7acbdce120e7'::uuid, NULL, 'd2ed72d8-090a-4efb-abe4-7acbdce120e7'::uuid, '1c114e8f-ccd0-42b8-a7fa-67fee61d4d18'::uuid, 'test', 'test'),
+    (true, 5, '2025-02-14 21:04:14.162', NULL, '2025-02-14 21:04:14.162', 'd2ed72d8-090a-4efb-abe4-7acbdce120e7'::uuid, NULL, 'd2ed72d8-090a-4efb-abe4-7acbdce120e5'::uuid, '1c114e8f-ccd0-42b8-a7fa-67fee61d4d15'::uuid, 'test', 'test'),
+    (true, 5, '2025-02-14 21:04:14.162', NULL, '2025-02-14 21:04:14.162', 'd2ed72d8-090a-4efb-abe4-7acbdce120e7'::uuid, NULL, 'd2ed72d8-090a-4efb-abe4-7acbdce120e6'::uuid, '1c114e8f-ccd0-42b8-a7fa-67fee61d4d14'::uuid, 'test', 'test'),
+    (true, 5, '2025-02-14 21:04:14.162', NULL, '2025-02-14 21:04:14.162', 'd2ed72d8-090a-4efb-abe4-7acbdce120e7'::uuid, NULL, 'd2ed72d8-090a-4efb-abe4-7acbdce120e5'::uuid, '1c114e8f-ccd0-42b8-a7fa-67fee61d4d13'::uuid, 'test', 'test');
