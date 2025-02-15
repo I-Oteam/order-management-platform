6b06b44e5537f78ad167dac60ebd47c7b7f6a077
HanaHww2 <hanahww2@gmail.com>
2025-02-15 14:46:41 +0900

    feat: review - 리뷰 생성하기 오류 수정#3
    - 리뷰 생성 요청의 잘못된 벨리데이션 어노테이션 수정

--- src/main/java/com/ioteam/order_management_platform/review/dto/CreateReviewRequestDto.java
+++ src/main/java/com/ioteam/order_management_platform/review/dto/CreateReviewRequestDto.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -16,10 +17,12 @@ public class CreateReviewRequestDto {
     @NotNull
     private UUID reviewOrderId;
     @NotNull
-    @Max(1000)
+    @Max(5)
     private int reviewScore;
     @NotNull
+    @Length(max = 1000)
     private String reviewContent;
     private String reviewImageUrl;
+    @NotNull
     private Boolean isPublic;
 }
