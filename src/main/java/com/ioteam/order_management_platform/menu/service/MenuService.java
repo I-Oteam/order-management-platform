2a77c5e13c4a3b4d4d7eb96978bdc0d4b3c2f988
Ryujy <bamdal124@gmail.com>
2025-02-17 16:31:31 +0900

    refactor: menu - 메뉴 등록 API 코드 수정
    - request dto 검증 어노테이션 추가
    - Entity 메서드 requestDto와 responseDto로 각각 옮겨서 구현
    - BaseEntity updatable 속성 추가

--- src/main/java/com/ioteam/order_management_platform/menu/service/MenuService.java
+++ src/main/java/com/ioteam/order_management_platform/menu/service/MenuService.java
@@ -1,27 +1,30 @@
 package com.ioteam.order_management_platform.menu.service;
 
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.ioteam.order_management_platform.menu.dto.CreateMenuRequestDto;
 import com.ioteam.order_management_platform.menu.dto.MenuResponseDto;
 import com.ioteam.order_management_platform.menu.entity.Menu;
 import com.ioteam.order_management_platform.menu.repository.MenuRepository;
+
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MenuService {
 
-    private final MenuRepository menuRepository;
+	private final MenuRepository menuRepository;
 
-    @Transactional
-    public MenuResponseDto createMenu(CreateMenuRequestDto requestDto) {
-        Menu menu = new Menu(requestDto);
-        Menu savedMenu = menuRepository.save(menu);
-        return savedMenu.toResponseDto();
-
-    }
+	@Transactional
+	public MenuResponseDto createMenu(CreateMenuRequestDto requestDto) {
+		UUID resId = requestDto.getResId();
+		// TODO: restaurant entity 생성 후 실제 존재하는지 확인
+		Menu menu = requestDto.toEntity(requestDto);
+		Menu savedMenu = menuRepository.save(menu);
+		return MenuResponseDto.fromEntity(savedMenu);
+	}
 }
