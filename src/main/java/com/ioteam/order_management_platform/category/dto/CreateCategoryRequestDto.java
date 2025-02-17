54ea544a420b6a276b021c93591373ba88b2b1ec
iconew123 <iconew123@gmail.com>
2025-02-17 16:57:44 +0900

    refactor: Category - 카테고리 생성 API 코드 리펙토링 #21
    - RequestDto클래스명 명확하게 수정

--- src/main/java/com/ioteam/order_management_platform/category/dto/CreateCategoryRequestDto.java
+++ src/main/java/com/ioteam/order_management_platform/category/dto/CreateCategoryRequestDto.java
@@ -1,14 +1,18 @@
 package com.ioteam.order_management_platform.category.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class CategoryRequestDto {
+public class CreateCategoryRequestDto {
 
+    @NotNull
+    @Length(max = 100)
     private String rcName;
 
 }
