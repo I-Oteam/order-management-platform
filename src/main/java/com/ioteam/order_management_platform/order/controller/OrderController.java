c68b1f5e9493a2cfac60ee5582298cc1ea6fa9b0
yeonzee <duswl1341@gmail.com>
2025-02-19 00:03:39 +0900

    feat: order - 주문 상세 정보 조회 기능 구현

--- src/main/java/com/ioteam/order_management_platform/order/controller/OrderController.java
+++ src/main/java/com/ioteam/order_management_platform/order/controller/OrderController.java
@@ -1,9 +1,10 @@
 package com.ioteam.order_management_platform.order.controller;
 
 import com.ioteam.order_management_platform.global.dto.CommonResponse;
-import com.ioteam.order_management_platform.order.dto.CreateOrderRequestDto;
-import com.ioteam.order_management_platform.order.dto.OrderListResponseDto;
-import com.ioteam.order_management_platform.order.dto.OrderResponseDto;
+import com.ioteam.order_management_platform.global.success.SuccessCode;
+import com.ioteam.order_management_platform.order.dto.req.CreateOrderRequestDto;
+import com.ioteam.order_management_platform.order.dto.res.OrderListResponseDto;
+import com.ioteam.order_management_platform.order.dto.res.OrderResponseDto;
 import com.ioteam.order_management_platform.order.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,16 +36,21 @@ public class OrderController {
 				.toUri();
 
 		return ResponseEntity.created(location)
-				.body(new CommonResponse<>(
-						"주문이 성공적으로 생성되었습니다.",
-						responseDto));
+				.body(new CommonResponse<>(SuccessCode.ORDER_CREATE, responseDto));
 	}
 
 	@Operation(summary = "전체 주문 조회")
 	@GetMapping("/all")
 	public ResponseEntity<CommonResponse<OrderListResponseDto>> getAllOrders() {
 		OrderListResponseDto responseDto = orderService.getAllOrders();
-		return ResponseEntity.ok(new CommonResponse<>("전체 주문 목록이 조회되었습니다.", responseDto));
+		return ResponseEntity.ok(new CommonResponse<>(SuccessCode.ORDER_ALL_INFO, responseDto));
+	}
+
+	@Operation(summary = "주문 상세 조회")
+	@GetMapping("/{order_id}")
+	public ResponseEntity<CommonResponse<OrderResponseDto>> getOrderDetail(@PathVariable("order_id") UUID orderId) {
+		OrderResponseDto responseDto = orderService.getOrderDetail(orderId);
+		return ResponseEntity.ok(new CommonResponse<>(SuccessCode.ORDER_DETAIL_INFO, responseDto));
 	}
 
 
