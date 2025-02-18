6405472c6ee80fff9dd4745a5869e97dd35a4157
iconew123 <iconew123@gmail.com>
2025-02-18 22:18:43 +0900

    feat: Category - 카테고리 모든조회 API #49
    - 빌더패턴으로 toEntity 생성

--- src/main/java/com/ioteam/order_management_platform/category/dto/CreateCategoryRequestDto.java
+++ src/main/java/com/ioteam/order_management_platform/category/dto/CreateCategoryRequestDto.java
@@ -2,20 +2,25 @@ package com.ioteam.order_management_platform.category.dto;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.ioteam.order_management_platform.category.entity.Category;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class CreateCategoryRequestDto {
 
 	@NotNull
 	@Length(max = 100)
 	private String rcName;
 
+	public Category toCategory(CreateCategoryRequestDto categoryRequestDto) {
+		return Category.builder()
+			.rcName(categoryRequestDto.getRcName())
+			.build();
+	}
 }
