53621816566640af5a50beb278066a64047724e8
HanaHww2 <hanahww2@gmail.com>
2025-02-19 01:01:16 +0900

    refactor: global - 시큐리티 유저 auditing 활성화

--- src/main/java/com/ioteam/order_management_platform/global/jpa/UserAuditorAware.java
+++ src/main/java/com/ioteam/order_management_platform/global/jpa/UserAuditorAware.java
@@ -1,26 +1,25 @@
 package com.ioteam.order_management_platform.global.jpa;
 
-import org.springframework.data.domain.AuditorAware;
-
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.ioteam.order_management_platform.user.security.UserDetailsImpl;
+
 public class UserAuditorAware implements AuditorAware<UUID> {
 
-    @Override
-    public Optional<UUID> getCurrentAuditor() {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	@Override
+	public Optional<UUID> getCurrentAuditor() {
 
-//        if(null == authentication || !authentication.isAuthenticated()) {
-//            return null;
-//        }
-//
-//        //사용자 환경에 맞게 로그인한 사용자의 정보를 불러온다.
-//        CustomUserDetails userDetails = (CustomUserDetails)authentication.getPrincipal();
-//
-//        return Optional.of(userDetails.getId());
-
-        return Optional.of(UUID.randomUUID());
-    }
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (null == authentication || !authentication.isAuthenticated()) {
+			return null;
+		}
+		UserDetailsImpl userDetails = (UserDetailsImpl)authentication.getPrincipal();
+		return Optional.of(userDetails.getUserId());
+	}
 
 }
