c6c7b59eddd673e55c906e2c26aefbaa0a3c2726
ybon1107 <leeyebon7929@gmail.com>
2025-02-19 17:39:20 +0900

    refactor : user - user requestDto 수정#80
    - requestDto 내부에 UserRoleEnum 형태로 수정

--- src/main/java/com/ioteam/order_management_platform/user/entity/UserRoleEnum.java
+++ src/main/java/com/ioteam/order_management_platform/user/entity/UserRoleEnum.java
@@ -1,24 +1,31 @@
 package com.ioteam.order_management_platform.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import lombok.Getter;
 
 @Getter
 public enum UserRoleEnum {
-    CUSTOMER(Authority.CUSTOMER),  // 사용자(고객) 권한
-    OWNER(Authority.OWNER),     // 사용자(가게주인) 권한
-    MANAGER(Authority.MANAGER),  // 관리자 권한
-    MASTER(Authority.MASTER);
+	CUSTOMER(Authority.CUSTOMER),  // 사용자(고객) 권한
+	OWNER(Authority.OWNER),     // 사용자(가게주인) 권한
+	MANAGER(Authority.MANAGER),  // 관리자 권한
+	MASTER(Authority.MASTER);
 
-    private final String authority;
+	private final String authority;
 
-    UserRoleEnum(String authority) {
-        this.authority = authority;
-    }
+	UserRoleEnum(String authority) {
+		this.authority = authority;
+	}
 
-    public static class Authority {
-        public static final String CUSTOMER = "ROLE_CUSTOMER";
-        public static final String OWNER = "ROLE_OWNER";
-        public static final String MANAGER = "ROLE_MANAGER";
-        public static final String MASTER = "ROLE_MASTER";
-    }
+	@JsonValue // JSON 변환 시 문자열 값으로 반환
+	public String getAuthority() {
+		return authority;
+	}
+
+	public static class Authority {
+		public static final String CUSTOMER = "ROLE_CUSTOMER";
+		public static final String OWNER = "ROLE_OWNER";
+		public static final String MANAGER = "ROLE_MANAGER";
+		public static final String MASTER = "ROLE_MASTER";
+	}
 }
