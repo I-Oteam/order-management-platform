c3d406c0c2b1f9b37f39c75a165c498fc642eef7
iconew123 <iconew123@gmail.com>
2025-02-21 01:17:43 +0900

    feat: Restaurant - Category에 연관관계 추가 #87
    - category와 restaurant 연관관계 추가

--- src/main/java/com/ioteam/order_management_platform/category/entity/Category.java
+++ src/main/java/com/ioteam/order_management_platform/category/entity/Category.java
@@ -1,15 +1,20 @@
 package com.ioteam.order_management_platform.category.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import com.ioteam.order_management_platform.category.dto.req.UpdateCategoryRequestDto;
 import com.ioteam.order_management_platform.global.entity.BaseEntity;
+import com.ioteam.order_management_platform.restaurant.entity.Restaurant;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -31,6 +36,9 @@ public class Category extends BaseEntity {
 
 	@Column(nullable = false, unique = true, length = 100)
 	private String rcName;
+
+	@OneToMany(mappedBy = "category", cascade = CascadeType.MERGE)
+	private List<Restaurant> restaurants = new ArrayList<>();
 
 	public void update(UpdateCategoryRequestDto updateCategoryDto) {
 		rcName = updateCategoryDto.getRcName();
