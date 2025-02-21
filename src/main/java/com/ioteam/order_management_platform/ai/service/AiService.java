890b662a2a2c581e8ed368a0df738c358b64fcbf
Ryujy <bamdal124@gmail.com>
2025-02-21 20:06:47 +0900

    refactor: AI 상품 설명 추천 기능 Exception 처리 #109
    - exception 처리 로직 추가
    - data.sql 파일 insert 순서 변경

--- src/main/java/com/ioteam/order_management_platform/ai/service/AiService.java
+++ src/main/java/com/ioteam/order_management_platform/ai/service/AiService.java
@@ -11,6 +11,7 @@ import com.ioteam.order_management_platform.ai.dto.req.RecommendDesRequestDto;
 import com.ioteam.order_management_platform.ai.dto.res.AnswerAiResponseDto;
 import com.ioteam.order_management_platform.ai.dto.res.GeminiResponseDto;
 import com.ioteam.order_management_platform.ai.entity.AIResponse;
+import com.ioteam.order_management_platform.ai.exception.AIException;
 import com.ioteam.order_management_platform.ai.repository.AiRepository;
 import com.ioteam.order_management_platform.global.exception.CustomApiException;
 import com.ioteam.order_management_platform.menu.exception.MenuException;
@@ -42,9 +43,7 @@ public class AiService {
 	public AnswerAiResponseDto recommendMenuDescription(UserDetailsImpl userDetails,
 		RecommendDesRequestDto requestDto) {
 
-		// 사용자 유효성 검증 (owner이면서 해당 가게 주인인지? 필요한가
 		User user = userDetails.getUser();
-
 		Restaurant restaurant = restaurantRepository.findByResIdAndDeletedAtIsNull(requestDto.getResId())
 			.orElseThrow(() -> new CustomApiException(MenuException.INVALID_RESTAURANT_ID));
 
@@ -55,11 +54,8 @@ public class AiService {
 
 		question += " 주어진 정보를 기반으로 <메뉴 설명> 내용을 추천해줘. 답변을 최대한 간결하게 50자 이하로 해줘.";
 
-		System.out.println(question);
-		// API 호출
 		AnswerAiResponseDto aiAnswer = requestGemini(question);
 
-		// AI의 응답 문장만 DB에 저장
 		AIResponse aiResponse = RecommendDesRequestDto.toEntity(user, question, aiAnswer);
 		AIResponse response = aiRepository.save(aiResponse);
 
@@ -67,7 +63,6 @@ public class AiService {
 	}
 
 	private AnswerAiResponseDto requestGemini(String question) {
-		//TODO: exceptio 처리
 		Map<String, Object> requestBody = Map.of(
 			"contents", new Object[] {
 				Map.of("parts", new Object[] {
@@ -76,14 +71,21 @@ public class AiService {
 			}
 		);
 
-		GeminiResponseDto aiAnswer = webClient.post()
-			.uri(geminiApiUrl + geminiApiKey)
-			.header("Content-Type", "application/json")
-			.bodyValue(requestBody)
-			.retrieve()
-			.bodyToMono(GeminiResponseDto.class)
-			.block();
+		try {
+			GeminiResponseDto aiAnswer = webClient.post()
+				.uri(geminiApiUrl + geminiApiKey)
+				.header("Content-Type", "application/json")
+				.bodyValue(requestBody)
+				.retrieve()
+				.bodyToMono(GeminiResponseDto.class)
+				.block();
 
-		return AnswerAiResponseDto.fromGemini(aiAnswer);
+			if (aiAnswer == null) {
+				throw new CustomApiException(AIException.INVALID_AI_RESPONSE);
+			}
+			return AnswerAiResponseDto.fromGemini(aiAnswer);
+		} catch (Exception e) {
+			throw new CustomApiException(AIException.AI_SERVICE_UNAVAILABLE);
+		}
 	}
 }
