2026ba62db2f039be101738d2b653279778fe8f8
ybon1107 <leeyebon7929@gmail.com>
2025-02-21 15:08:23 +0900

    refactor : 사용자 - 회원가입 리팩토링#115
    - 회원가입 requestDto 내에 정규식을 활용한 필드 조건 설정

--- src/main/java/com/ioteam/order_management_platform/user/dto/req/SignupRequestDto.java
+++ src/main/java/com/ioteam/order_management_platform/user/dto/req/SignupRequestDto.java
@@ -6,6 +6,8 @@ import com.ioteam.order_management_platform.user.entity.UserRoleEnum;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,13 +18,24 @@ import lombok.Getter;
 public class SignupRequestDto {
 	@NotBlank(message = "nickname은 공백일 수 없습니다.")
 	private String nickname;
+
 	@NotBlank(message = "username은 공백일 수 없습니다.")
+	@Size(min = 4, max = 10, message = "사용자명은 4~10자 사이여야 합니다.")
+	@Pattern(regexp = "^[a-z0-9]{4,11}$", message = "사용자명은 알파벳 소문자(a-z)와 숫자(0-9)만 포함해야 합니다.")
 	private String username;
+
 	@NotBlank(message = "password는 공백일 수 없습니다.")
+	@Size(min = 8, max = 15, message = "비밀번호는 8~15자 사이여야 합니다.")
+	@Pattern(
+		regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
+		message = "비밀번호는 최소 하나의 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다."
+	)
 	private String password;
+
 	@Email(message = "올바른 이메일 형식이 아닙니다.")
 	@NotBlank(message = "email은 공백일 수 없습니다.")
 	private String email;
+
 	@NotNull(message = "권한을 선택해주세요.")
 	private UserRoleEnum role;
 	private String masterToken = "";
