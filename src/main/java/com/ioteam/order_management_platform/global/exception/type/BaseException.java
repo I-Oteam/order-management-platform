37af8690c059dacf778a484e02a3905d37cbf935
ybon1107 <leeyebon7929@gmail.com>
2025-02-21 15:53:58 +0900

    refactor : 사용자 - 회원가입 리팩토링#115
    - 중복된 email, nickname, username 에 대한 exception 처리

--- src/main/java/com/ioteam/order_management_platform/global/exception/type/BaseException.java
+++ src/main/java/com/ioteam/order_management_platform/global/exception/type/BaseException.java
@@ -4,28 +4,34 @@ import org.springframework.http.HttpStatus;
 
 public enum BaseException implements ExceptionType {
 
-    SERVER_ERROR(HttpStatus.BAD_REQUEST, "서버 오류가 발생하였습니다.", "E_NOT_DEFINED"),
-    INVALID_INPUT(HttpStatus.BAD_REQUEST, "유효성 검증에 실패했습니다.", "E_VALIDATION"),
-    NULL_DATA(HttpStatus.BAD_REQUEST, "널 데이터가 존재해서는 안 됩니다.", "E_NULL"),
-    UNAUTHORIZED_REQ(HttpStatus.FORBIDDEN, "권한이 없는 요청입니다.", "E_UNAUTH");
+	SERVER_ERROR(HttpStatus.BAD_REQUEST, "서버 오류가 발생하였습니다.", "E_NOT_DEFINED"),
+	INVALID_INPUT(HttpStatus.BAD_REQUEST, "유효성 검증에 실패했습니다.", "E_VALIDATION"),
+	NULL_DATA(HttpStatus.BAD_REQUEST, "널 데이터가 존재해서는 안 됩니다.", "E_NULL"),
+	UNAUTHORIZED_REQ(HttpStatus.FORBIDDEN, "권한이 없는 요청입니다.", "E_UNAUTH"),
+	DUPLICATE_FIELD(HttpStatus.CONFLICT, "중복된 데이터가 존재합니다.", "E_DUPLICATE");
 
-    private final HttpStatus status;
-    private final String message;
-    private final String errorCode;
+	private final HttpStatus status;
+	private final String message;
+	private final String errorCode;
 
-    BaseException(HttpStatus status, String message, String errorCode) {
-        this.status = status;
-        this.message = message;
-        this.errorCode = errorCode;
-    }
+	BaseException(HttpStatus status, String message, String errorCode) {
+		this.status = status;
+		this.message = message;
+		this.errorCode = errorCode;
+	}
 
-    @Override
-    public HttpStatus getStatus() { return this.status; }
-    @Override
-    public String getMessage() {
-        return this.message;
-    }
+	@Override
+	public HttpStatus getStatus() {
+		return this.status;
+	}
 
-    @Override
-    public String getErrorCode() { return this.errorCode; }
+	@Override
+	public String getMessage() {
+		return this.message;
+	}
+
+	@Override
+	public String getErrorCode() {
+		return this.errorCode;
+	}
 }
