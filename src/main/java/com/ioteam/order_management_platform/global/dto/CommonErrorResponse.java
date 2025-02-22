10b6f8985172b90a5be869eed4bdd92cc033c92f
HanaHww2 <hanahww2@gmail.com>
2025-02-22 11:41:16 +0900

    feat: 공통 - 공통 리팩토링 #143
    - 글로벌 익셉션, 에러 응답 코드 수정

--- src/main/java/com/ioteam/order_management_platform/global/dto/CommonErrorResponse.java
+++ src/main/java/com/ioteam/order_management_platform/global/dto/CommonErrorResponse.java
@@ -1,15 +1,40 @@
 package com.ioteam.order_management_platform.global.dto;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.ioteam.order_management_platform.global.exception.type.ExceptionType;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 public class CommonErrorResponse {
-    private String message;
-    private String errorCode;
+	private String message;
+	private String errorCode;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private List<ErrorField> errorFields;
 
-    public CommonErrorResponse(String message, ExceptionType exceptionType) {
-        this.message = message;
-        this.errorCode = exceptionType.getErrorCode();
-    }
+	public CommonErrorResponse(ExceptionType exceptionType) {
+		this.message = exceptionType.getMessage();
+		this.errorCode = exceptionType.getErrorCode();
+	}
+
+	public CommonErrorResponse(String message, ExceptionType exceptionType) {
+		this.message = message;
+		this.errorCode = exceptionType.getErrorCode();
+	}
+
+	public CommonErrorResponse(ExceptionType exceptionType, List<ErrorField> errorFields) {
+		this.message = exceptionType.getMessage();
+		this.errorCode = exceptionType.getErrorCode();
+		this.errorFields = errorFields;
+	}
+
+	@AllArgsConstructor
+	@Getter
+	public static class ErrorField {
+		private String field;
+		private String message;
+	}
 }
