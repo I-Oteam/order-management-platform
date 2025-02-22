a2caf3e03338840c59908c369054e5eea14358d5
Ryujy <bamdal124@gmail.com>
2025-02-22 21:17:53 +0900

    style: RestClient Config 분리로 필요없어진 코드 제거 #150

--- src/main/java/com/ioteam/order_management_platform/global/config/RestClientConfig.java
+++ src/main/java/com/ioteam/order_management_platform/global/config/RestClientConfig.java
@@ -1,6 +1,5 @@
 package com.ioteam.order_management_platform.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -9,9 +8,6 @@ import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
-
-	@Value("${gemini.api.url}")
-	private String geminiApiUrl;
 
 	@Bean
 	public RestClient restClient() {
