b50731290955ccf02430be63f72f7b9c7346dad2
Ryujy <bamdal124@gmail.com>
2025-02-25 10:50:56 +0900

    test: modifyMenu 권한별 성공 통합 테스트 작성 #174

--- src/main/java/com/ioteam/order_management_platform/menu/dto/req/UpdateMenuRequestDto.java
+++ src/main/java/com/ioteam/order_management_platform/menu/dto/req/UpdateMenuRequestDto.java
@@ -8,10 +8,12 @@ import com.ioteam.order_management_platform.menu.entity.MenuStatus;
 
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class UpdateMenuRequestDto {
 
 	@Length(min = 1, max = 100)
--- src/test/java/com/ioteam/order_management_platform/menu/controller/MenuIntegrationTest.java
+++ src/test/java/com/ioteam/order_management_platform/menu/controller/MenuIntegrationTest.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ioteam.order_management_platform.menu.dto.req.CreateMenuRequestDto;
+import com.ioteam.order_management_platform.menu.dto.req.UpdateMenuRequestDto;
 import com.ioteam.order_management_platform.menu.entity.MenuStatus;
 import com.ioteam.order_management_platform.utils.security.WithMockCustomUser;
 
@@ -56,11 +57,16 @@ class MenuIntegrationTest {
 			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
 			.andExpect(jsonPath("$.result.rmName")
 				.value("test"))
-			.andExpect(jsonPath("$.result.rmPrice").value(BigDecimal.valueOf(10000)))
-			.andExpect(jsonPath("$.result.rmImageUrl").value("test"))
-			.andExpect(jsonPath("$.result.rmDescription").value("test"))
-			.andExpect(jsonPath("$.result.rmStatus").value(MenuStatus.ON_SALE.name()))
-			.andExpect(jsonPath("$.result.isPublic").value(true))
+			.andExpect(jsonPath("$.result.rmPrice")
+				.value(BigDecimal.valueOf(10000)))
+			.andExpect(jsonPath("$.result.rmImageUrl")
+				.value("test"))
+			.andExpect(jsonPath("$.result.rmDescription")
+				.value("test"))
+			.andExpect(jsonPath("$.result.rmStatus")
+				.value(MenuStatus.ON_SALE.name()))
+			.andExpect(jsonPath("$.result.isPublic")
+				.value(true))
 			.andDo(print());
 	}
 
@@ -125,6 +131,64 @@ class MenuIntegrationTest {
 				.value("열리오올리오"))
 			.andExpect(jsonPath("$.result.rmStatus")
 				.value("SOLD_OUT"))
+			.andDo(print());
+	}
+
+	@WithMockCustomUser(userId = "d2ed72d8-090a-4efb-abe4-7acbdce120e3", role = "MANAGER")
+	@DisplayName("매니저_상품 수정 성공 200")
+	@Test
+	void modifyMenu_Manager_200() throws Exception {
+		// given
+		UUID menuId = UUID.fromString("439f222b-0cbb-4600-a989-e7fdabf120d6");
+		UpdateMenuRequestDto requestDto = UpdateMenuRequestDto.builder()
+			.rmName("test")
+			.rmPrice(BigDecimal.valueOf(0))
+			.rmStatus(MenuStatus.ON_SALE)
+			.build();
+
+		// when, then
+		mockMvc.perform(
+				patch("/api/menus/{menu_id}", menuId)
+					.header("Authorization", "Bearer {ACCESS_TOKEN}")
+					.content(objectMapper.writeValueAsString(requestDto))
+					.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.result.rmName")
+				.value("test"))
+			.andExpect(jsonPath("$.result.rmPrice")
+				.value(BigDecimal.valueOf(0)))
+			.andExpect(jsonPath("$.result.rmStatus")
+				.value("ON_SALE"))
+			.andDo(print());
+	}
+
+	@WithMockCustomUser(userId = "d2ed72d8-090a-4efb-abe4-7acbdce120e2", role = "OWNER")
+	@DisplayName("가게주인_상품 수정 성공 200")
+	@Test
+	void modifyMenu_Owner_200() throws Exception {
+		// given
+		UUID menuId = UUID.fromString("439f222b-0cbb-4600-a989-e7fdabf120d6");
+		UpdateMenuRequestDto requestDto = UpdateMenuRequestDto.builder()
+			.rmName("Owner_test")
+			.rmPrice(BigDecimal.valueOf(10))
+			.rmStatus(MenuStatus.HIDDEN)
+			.build();
+
+		// when, then
+		mockMvc.perform(
+				patch("/api/menus/{menu_id}", menuId)
+					.header("Authorization", "Bearer {ACCESS_TOKEN}")
+					.content(objectMapper.writeValueAsString(requestDto))
+					.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.result.rmName")
+				.value("Owner_test"))
+			.andExpect(jsonPath("$.result.rmPrice")
+				.value(BigDecimal.valueOf(10)))
+			.andExpect(jsonPath("$.result.rmStatus")
+				.value("HIDDEN"))
 			.andDo(print());
 	}
 }
