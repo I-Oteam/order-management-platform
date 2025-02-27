ba7ea95859f09a9e8c9cb5e38d59cce2e920644d
ybon1107 <leeyebon7929@gmail.com>
2025-02-27 22:01:05 +0900

    refactor : 결제 - 접근권한별 서비스 처리 로직#179
    - getPayment 서비스 접근 권한 조건 처리 코드 주석 제거

--- src/main/java/com/ioteam/order_management_platform/payment/service/PaymentService.java
+++ src/main/java/com/ioteam/order_management_platform/payment/service/PaymentService.java
@@ -38,44 +38,6 @@ public class PaymentService {
 	private final RestaurantRepository restaurantRepository;
 	private final PaymentRetrieveStrategies paymentRetrieveStrategist;
 
-	public PaymentResponseDto getPayment(UUID paymentId, UserDetailsImpl userDetails) {
-		UserRoleEnum role = userDetails.getRole();
-
-		return paymentRetrieveStrategist.stream()
-			.filter(it -> it.isSupport(role))
-			.findAny()
-			.orElseThrow(() -> new CustomApiException(PaymentException.UNAUTHORIZED_PAYMENT_ACCESS))
-			.getPayment(paymentId, userDetails);
-
-	}
-
-	// // MASTER, MANAGER: 모든 결제 내역 조회 가능
-	// if (userDetails.getRole() == UserRoleEnum.MASTER || userDetails.getRole() == UserRoleEnum.MANAGER) {
-	// 	Payment payment = paymentRepository.findByPaymentIdAndDeletedAtIsNull(paymentId)
-	// 		.orElseThrow(() -> new CustomApiException(PaymentException.INVALID_PAYMENT_ID));
-	// 	return PaymentResponseDto.from(payment);
-	// }
-	//
-	// // CUSTOMER: 본인이 주문한 결제 내역만 조회 가능
-	// if (userDetails.getRole() == UserRoleEnum.CUSTOMER) {
-	// 	Payment payment = paymentRepository.findPaymentForCustomer(paymentId, userDetails.getUserId())
-	// 		.orElseThrow(() -> new CustomApiException(PaymentException.UNAUTHORIZED_PAYMENT_ACCESS));
-	// 	return PaymentResponseDto.from(payment);
-	// }
-	//
-	// // OWNER: 본인 가게의 주문 결제 내역만 조회 가능
-	// if (userDetails.getRole() == UserRoleEnum.OWNER) {
-	// 	Payment payment = paymentRepository.findPaymentWithOrderAndRestaurant(paymentId)
-	// 		.orElseThrow(() -> new CustomApiException(PaymentException.INVALID_PAYMENT_ID));
-	//
-	// 	// 본인이 해당 가게의 주인인지 확인
-	// 	if (!payment.getOrder().getRestaurant().getOwner().getUserId().equals(userDetails.getUserId())) {
-	// 		throw new CustomApiException(PaymentException.UNAUTHORIZED_PAYMENT_ACCESS);
-	// 	}
-	// 	return PaymentResponseDto.from(payment);
-	// }
-	//
-	// throw new CustomApiException(PaymentException.UNAUTHORIZED_PAYMENT_ACCESS);
 	@Transactional
 	public PaymentResponseDto createPayment(UserDetailsImpl userDetails, CreatePaymentRequestDto requestDto) {
 		// 주문이 유효한지, 주문자가 본인인지, 주문이 삭제되지 않았는지 확인
@@ -97,6 +59,17 @@ public class PaymentService {
 		Page<AdminPaymentResponseDto> paymentDtoPage = paymentRepository.searchPaymentAdminByCondition(condition,
 			pageable);
 		return new CommonPageResponse<>(paymentDtoPage);
+	}
+
+	public PaymentResponseDto getPayment(UUID paymentId, UserDetailsImpl userDetails) {
+		UserRoleEnum role = userDetails.getRole();
+
+		return paymentRetrieveStrategist.stream()
+			.filter(it -> it.isSupport(role))
+			.findAny()
+			.orElseThrow(() -> new CustomApiException(PaymentException.UNAUTHORIZED_PAYMENT_ACCESS))
+			.getPayment(paymentId, userDetails);
+
 	}
 
 	public CommonPageResponse<PaymentResponseDto> searchPaymentByUser(CustomerPaymentSearchCondition condition,
