99908267dcd83b0cd47143fb928183e4605758ec
HanaHww2 <hanahww2@gmail.com>
2025-02-28 23:06:39 +0900

    refactor: global - QuerydslUtil 보완 #201

--- src/main/java/com/ioteam/order_management_platform/global/utils/QuerydslUtil.java
+++ src/main/java/com/ioteam/order_management_platform/global/utils/QuerydslUtil.java
@@ -2,10 +2,15 @@ package com.ioteam.order_management_platform.global.utils;
 
 import java.util.function.Supplier;
 
+import com.ioteam.order_management_platform.global.exception.CustomApiException;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 
 public class QuerydslUtil {
+	private QuerydslUtil() {
+		throw new CustomApiException("Utility Class");
+	}
+
 	public static BooleanBuilder nullSafeBuilder(Supplier<BooleanExpression> f) {
 		try {
 			return new BooleanBuilder(f.get());
